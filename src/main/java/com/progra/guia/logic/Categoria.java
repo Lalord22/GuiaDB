f2939a4e6e6c4f3cf7dcb0fcc36699563a321d4f
Jennifer Lobo V... <101234566+JenniLoboV@users.noreply.github.com>
2023-04-17 23:19:23 -0600

    Clase categoria(Faltan gets y set, hay que revisar los atributos)

---
+++ src/main/java/com/progra/guia/logic/Categoria.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.progra.guia.logic;
+
+/**
+ *
+ * @author Jennifer Lobo
+ */
+public class Categoria {
+    String id;
+    String descripcion;
+
+    public Categoria() {
+    }
+
+    public Categoria(String id, String descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+    }
+    
+}
